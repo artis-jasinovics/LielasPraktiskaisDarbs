498afd35cc52008c974fe0de7d0f0ae9b6464835
artis-jasinovics <artis200028@gmail.com>
2020-06-14 00:21:19 +0300

    Completed the groundwork for the website (created controllers, defined routes).

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::resource('cars', 'CarController');
+
+Route::resource('owners', 'UserController');
+
+Route::resource('reviews', 'ReviewController');
+
+Route::resource('rents', 'RentController');
